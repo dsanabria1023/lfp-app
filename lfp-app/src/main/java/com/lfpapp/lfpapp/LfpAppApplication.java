e170bea405049a9186c2c2a43b076f6081b076b5
Derek Sanabria <DerekSanabria@gmail.com>
2018-08-04 13:58:18 -0500

    Initial commit

---
+++ lfp-app/src/main/java/com/lfpapp/lfpapp/LfpAppApplication.java
@@ -0,0 +1,12 @@
+package com.lfpapp.lfpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfpAppApplication.class, args);
+	}
+}
---
+++ lfp-app/src/test/java/com/lfpapp/lfpapp/LfpAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lfpapp.lfpapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LfpAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
